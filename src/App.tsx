50ae2ecd51e20b0c4bcfbd7f7cc80faa1ca96cbe
hilee-hdxwill <140031116+hilee-hdxwill@users.noreply.github.com>
2024-11-15 16:28:30 +0900

    feat: load mpr view

--- src/App.tsx
+++ src/App.tsx
@@ -1,91 +1,170 @@
-import { useEffect,  useRef } from "react"
-import createImageIdsAndCacheMetaData  from "./lib/createImageIdsAndCacheMetaData"
-import { RenderingEngine, Enums, type Types, volumeLoader, cornerstoneStreamingImageVolumeLoader } from "@cornerstonejs/core"
-import {init as csRenderInit} from "@cornerstonejs/core"
-import {init as csToolsInit} from "@cornerstonejs/tools"
-import {init as dicomImageLoaderInit} from "@cornerstonejs/dicom-image-loader"
+import { useEffect, useRef, useState } from "react";
+import {
+  RenderingEngine,
+  Enums,
+  volumeLoader,
+  cornerstoneStreamingImageVolumeLoader,
+  setVolumesForViewports,
+} from "@cornerstonejs/core";
 
+import { init as coreInit } from "@cornerstonejs/core";
+import { init as csToolsInit } from "@cornerstonejs/tools";
+import { init as dicomImageLoaderInit } from "@cornerstonejs/dicom-image-loader";
+// import createImageIdsAndCacheMetaData from "./lib/createImageIdsAndCacheMetaData";
 
-volumeLoader.registerUnknownVolumeLoader(
-  cornerstoneStreamingImageVolumeLoader 
-)
+volumeLoader.registerUnknownVolumeLoader(cornerstoneStreamingImageVolumeLoader);
 
 function App() {
-  const elementRef = useRef<HTMLDivElement>(null)
-  const running = useRef(false)
+  const axialElement = useRef<HTMLDivElement>(null);
+  const coronalElment = useRef<HTMLDivElement>(null);
+  const sagittalElement = useRef<HTMLDivElement>(null);
+  const running = useRef(false);
+  const [imageIds, setImageIds] = useState<string[]>([]);
 
   useEffect(() => {
     const setup = async () => {
       if (running.current) {
-        return
+        return;
       }
-      running.current = true
-      
-      await csRenderInit()
-      await csToolsInit()
-      dicomImageLoaderInit({maxWebWorkers:1})
+      running.current = true;
 
-      // Get Cornerstone imageIds and fetch metadata into RAM
-      const imageIds = await createImageIdsAndCacheMetaData({
-        StudyInstanceUID:
-          "1.3.6.1.4.1.14519.5.2.1.7009.2403.334240657131972136850343327463",
-        SeriesInstanceUID:
-          "1.3.6.1.4.1.14519.5.2.1.7009.2403.226151125820845824875394858561",
-        wadoRsRoot: "https://d3t6nz73ql33tx.cloudfront.net/dicomweb",
-      })
+      await coreInit();
+      await csToolsInit();
+      await dicomImageLoaderInit({ maxWebWorkers: 1 });
+
+      // const imageIds = await createImageIdsAndCacheMetaData({
+      //   StudyInstanceUID:
+      //     "1.3.6.1.4.1.14519.5.2.1.7009.2403.334240657131972136850343327463",
+      //   SeriesInstanceUID:
+      //     "1.3.6.1.4.1.14519.5.2.1.7009.2403.226151125820845824875394858561",
+      //   wadoRsRoot: "https://d3t6nz73ql33tx.cloudfront.net/dicomweb",
+      // });
 
       // Instantiate a rendering engine
-      const renderingEngineId = "myRenderingEngine"
-      const renderingEngine = new RenderingEngine(renderingEngineId)
-      const viewportId = "CT"
+      const renderingEngineId = "myRenderingEngine";
+      const renderingEngine = new RenderingEngine(renderingEngineId);
+      const viewportId1 = "CT_AXIAL";
+      const viewportId2 = "CT_CHORONAL";
+      const viewportId3 = "CT_SAGITTAL";
 
-
-      const viewportInput = {
-        viewportId,
-        type: Enums.ViewportType.ORTHOGRAPHIC,
-        element: elementRef.current,
-        defaultOptions: {
-          orientation: Enums.OrientationAxis.SAGITTAL,
+      const viewportInput = [
+        {
+          viewportId: viewportId1,
+          type: Enums.ViewportType.ORTHOGRAPHIC,
+          element: coronalElment.current,
+          defaultOptions: {
+            orientation: Enums.OrientationAxis.AXIAL,
+          },
         },
-      }
+        {
+          viewportId: viewportId2,
+          type: Enums.ViewportType.ORTHOGRAPHIC,
+          element: axialElement.current,
+          defaultOptions: {
+            orientation: Enums.OrientationAxis.CORONAL,
+          },
+        },
+        {
+          viewportId: viewportId3,
+          type: Enums.ViewportType.ORTHOGRAPHIC,
+          element: sagittalElement.current,
+          defaultOptions: {
+            orientation: Enums.OrientationAxis.SAGITTAL,
+          },
+        },
+      ];
 
-      renderingEngine.enableElement(viewportInput)
+      const volumeId = "myVolume";
 
-      // Get the stack viewport that was created
-      const viewport = renderingEngine.getViewport(viewportId) as Types.IVolumeViewport
-
-      // Define a volume in memory
-      const volumeId = "streamingImageVolume"
       const volume = await volumeLoader.createAndCacheVolume(volumeId, {
         imageIds,
-      })
+      });
+
+      renderingEngine.setViewports(viewportInput);
 
       // Set the volume to load
-      // @ts-ignore
-      volume.load()
+      volume.load();
 
-      // Set the volume on the viewport and it's default properties
-      viewport.setVolumes([{ volumeId}])
+      setVolumesForViewports(
+        renderingEngine,
+        [{ volumeId }],
+        [viewportId1, viewportId2, viewportId3]
+      );
 
-      // Render the image
-      viewport.render()
-    }
+      renderingEngine.renderViewports([viewportId1, viewportId2, viewportId3]);
+    };
 
-    setup()
+    if (imageIds.length > 0) setup();
 
-    // Create a stack viewport
-  }, [elementRef, running])
+    // Create a stack viewport //stack을 만들면 안 되지! => 이것도 확인이 필요하다!
+  }, [coronalElment, sagittalElement, axialElement, running, imageIds]);
+
+  const handleFileUpload = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const files = event.target.files;
+    if (!files || files.length === 0) return;
+
+    const newImageIds: string[] = [];
+    Array.from(files).forEach((file) => {
+      const blobUrl = URL.createObjectURL(file);
+      const imageId = `wadouri:${blobUrl}`;
+      newImageIds.push(imageId);
+    });
+    setImageIds(newImageIds);
+  };
+
+  // const handleFileUpload = async (
+  //   event: React.ChangeEvent<HTMLInputElement>
+  // ) => {
+  //   const files = event.target.files;
+  //   if (!files || files.length === 0) return;
+
+  //   const newImageIds: string[] = [];
+  //   for (const file of Array.from(files)) {
+  //     // File을 Blob으로 변환하여 cornerstone의 imageId 생성
+  //     const blobUrl = URL.createObjectURL(file);
+  //     const imageId = `wadouri:${blobUrl}`;
+  //     newImageIds.push(imageId);
+  //   }
+  //   setImageIds(newImageIds);
+  // };
 
   return (
-    <div
-      ref={elementRef}
-      style={{
-        width: "512px",
-        height: "512px",
-        backgroundColor: "#000",
-      }}
-    ></div>
-  )
+    <>
+      <input
+        type="file"
+        multiple
+        onChange={handleFileUpload}
+        accept=".dcm"
+        style={{ marginBottom: "10px" }}
+      />
+      <div>
+        <div
+          ref={coronalElment}
+          style={{
+            width: "512px",
+            height: "512px",
+            backgroundColor: "#000",
+          }}
+        ></div>
+        <div
+          ref={axialElement}
+          style={{
+            width: "512px",
+            height: "512px",
+            backgroundColor: "#000",
+          }}
+        ></div>
+        <div
+          ref={sagittalElement}
+          style={{
+            width: "512px",
+            height: "512px",
+            backgroundColor: "#000",
+          }}
+        ></div>
+      </div>
+    </>
+  );
 }
 
-export default App
+export default App;
--- src/lib/createImageIdsAndCacheMetaData.ts
+++ src/lib/createImageIdsAndCacheMetaData.ts
@@ -1,14 +1,12 @@
-import { api } from "dicomweb-client"
-import cornerstoneDICOMImageLoader from "@cornerstonejs/dicom-image-loader"
+import { api } from "dicomweb-client";
+import cornerstoneDICOMImageLoader from "@cornerstonejs/dicom-image-loader";
 
 /**
 /**
  * Uses dicomweb-client to fetch metadata of a study, cache it in cornerstone,
  * and return a list of imageIds for the frames.
- *
  * Uses the app config to choose which study to fetch, and which
  * dicom-web server to fetch it from.
- *
  * @returns {string[]} An array of imageIds for instances in the study.
  */
 
@@ -19,24 +17,24 @@ export default async function createImageIdsAndCacheMetaData({
   wadoRsRoot,
   client = null,
 }) {
-  const SOP_INSTANCE_UID = "00080018"
-  const SERIES_INSTANCE_UID = "0020000E"
+  const SOP_INSTANCE_UID = "00080018";
+  const SERIES_INSTANCE_UID = "0020000E";
 
   const studySearchOptions = {
     studyInstanceUID: StudyInstanceUID,
     seriesInstanceUID: SeriesInstanceUID,
-  }
+  };
 
   client =
     client ||
-    new api.DICOMwebClient({ url: wadoRsRoot as string, singlepart: true })
-  const instances = await client.retrieveSeriesMetadata(studySearchOptions)
+    new api.DICOMwebClient({ url: wadoRsRoot as string, singlepart: true });
+  const instances = await client.retrieveSeriesMetadata(studySearchOptions);
   const imageIds = instances.map((instanceMetaData) => {
-    const SeriesInstanceUID = instanceMetaData[SERIES_INSTANCE_UID].Value[0]
+    const SeriesInstanceUID = instanceMetaData[SERIES_INSTANCE_UID].Value[0];
     const SOPInstanceUIDToUse =
-      SOPInstanceUID || instanceMetaData[SOP_INSTANCE_UID].Value[0]
+      SOPInstanceUID || instanceMetaData[SOP_INSTANCE_UID].Value[0];
 
-    const prefix = "wadors:"
+    const prefix = "wadors:";
 
     const imageId =
       prefix +
@@ -47,17 +45,17 @@ export default async function createImageIdsAndCacheMetaData({
       SeriesInstanceUID +
       "/instances/" +
       SOPInstanceUIDToUse +
-      "/frames/1"
+      "/frames/1";
 
     cornerstoneDICOMImageLoader.wadors.metaDataManager.add(
       imageId,
       instanceMetaData
-    )
-    return imageId
-  })
+    );
+    return imageId;
+  });
 
   // we don't want to add non-pet
   // Note: for 99% of scanners SUV calculation is consistent bw slices
 
-  return imageIds
+  return imageIds;
 }
